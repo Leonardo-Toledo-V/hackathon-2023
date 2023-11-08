5fc508c88054ed7253695d4db87e45920327ddc5
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-11-08 12:32:43 -0600

    feat(src): Testing tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 function App() {
 
   return (
-    <>
+    <main className='bg-[#EBE0D4] h-screen w-full'>
       <div>
         <p className='bg-red-500'> Hello world from vite and react and tailwind</p>
       </div>
-    </>
+    </main>
   )
 }
 
