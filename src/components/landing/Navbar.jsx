abed5443dfe574e56f60fde65b295197b625e876
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2023-11-09 13:17:37 -0600

    feat(landing):navbar

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -3,30 +3,25 @@ import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@n
 
 export default function Header() {
     return (
-        <Navbar className="bg-[#0F0F0F] hidden sm:block" position="static">
-            <NavbarContent justify="start" >
-                <NavbarBrand >
+        <Navbar position="static" className="bg-[#0F0F0F] font-lato">
+            <NavbarBrand>
+                <Link color="foreground" href="/" className="text-[#ffffffca]">
                     <img
                         className="w-10"
                         src="eye.svg" />
-                </NavbarBrand>
-                <NavbarContent className="flex sm:flex gap-12 font-lato">
-                    <NavbarItem>
-                        <Link href="/lists" className="text-[#ffffffca]">
-                            Secretarias
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem>
-                        <Link href="/activities" className="text-[#ffffffca]">
-                            Actividades
-                        </Link>
-                    </NavbarItem>
-                    <NavbarItem>
-                        <Link href="https://www.youtube.com/watch?v=fGIWsQDif_M&ab_channel=PesoPluma" className="text-[#ffffffca]">
-                            Integrations
-                        </Link>
-                    </NavbarItem>
-                </NavbarContent>
+                </Link>
+            </NavbarBrand>
+            <NavbarContent className="hidden sm:flex gap-12" justify="center">
+                <NavbarItem>
+                    <Link color="foreground" href="/lists" className="text-[#ffffffca]">
+                        Secretarias
+                    </Link>
+                </NavbarItem>
+                <NavbarItem isActive>
+                    <Link href="/activities" className="text-[#ffffffca]">
+                        Actividades
+                    </Link>
+                </NavbarItem>
             </NavbarContent>
             <NavbarContent justify="end">
                 <NavbarItem>
