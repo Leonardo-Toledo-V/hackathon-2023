0fa19516e120eb63e7a48a617e027b2a28f6a849
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-11-09 02:53:45 -0600

    feat(components): Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/landing/Navbar'
 import Main from './components/landing/Main'
+import Footer from './components/footer/Footer'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <main className='bg-[#0F0F0F] h-full w-full'>
         <Header/>
         <Main/>
+        <Footer/>
     </main>
   )
 }
