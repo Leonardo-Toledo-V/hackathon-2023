8c229736526eea7452f5c7b10e60db41615c81d9
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-11-08 20:28:27 -0600

    feat(components): Photos changesd

--- src/components/landing/Main.jsx
+++ src/components/landing/Main.jsx
@@ -10,7 +10,7 @@ export default function Main() {
                 </div>
                 <div className="bg-[url('/img1.png')] xl:h-[30rem] bg-center bg-cover xl:flex" >
                     <div className='xl:max-w-5xl pt-2'>
-                        <h2 className='xl:text-[5rem] leading- font-inter text-white mx-6 tracking-widest text-center xl:text-left  '>TRANSPARENCIA EN CHIAPAS</h2>
+                        <h2 className='xl:text-[5rem] leading-none font-inter text-white mx-6 tracking-widest text-center xl:text-left  '>TRANSPARENCIA EN CHIAPAS</h2>
                     </div>
                     <div className='text-white font-lato text-xl xl:self-end xl:order-1 pb-12'>
                         <p className='xl:max-w-sm'>Unidos por Chiapas, transformando la gobernanza con transparencia y apertura para un futuro más justo y equitativo</p>
@@ -19,8 +19,8 @@ export default function Main() {
             </div>
             <div>
                 <div className='flex justify-start mt-24'>
-                    <div className='max-w-xl'>
-                        <img src="img1.png" alt="" />
+                    <div className='w-[37rem] h-[30rem]'>
+                        <img className='object-cover w-full h-full' src="img2.png" alt="" />
                     </div>
                     <div className='flex flex-col ml-24 mt-12'>
                         <div>
@@ -51,8 +51,8 @@ export default function Main() {
                             <p className='mt-12 text-[#2c2c2cb7] max-w-3xl font-lato text-xl'>Nuestra misión es mostrar al pueblo chiapaneco cómo se practica la justicia y cómo pueden ejercer su voz y voto. Queremos ser un puente entre las instituciones de justicia y la comunidad, proporcionando educación y recursos para que las personas entiendan sus derechos y participen en la toma de decisiones que los afectan. <br/><br/>Chiapas merece una justicia accesible y participativa, y trabajamos para hacerlo posible.</p>
                         </div>
                     </div>
-                    <div className='max-w-xl'>
-                        <img src="img1.png" alt="" />
+                    <div className='w-[45rem] h-[25rem]'>
+                        <img className='object-cover w-full h-full' src="img3.png" alt="" />
                     </div>
                 </div>
             </div>
