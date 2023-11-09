d8167fd0f0f1079dddc1e6771e511073d1b71ff4
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-11-08 22:26:01 -0600

    feat(components): View changed

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -4,8 +4,8 @@ import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@n
 
 export default function Header() {
     return (
-        <Navbar isBordered className="bg-[#0f2923]">
-            <NavbarContent justify="start" className="brightness-50">
+        <Navbar className="bg-[#EBE0D4]" shouldHideOnScroll>
+            <NavbarContent justify="start" >
                 <NavbarBrand >
                     <img
                         className="w-14"
@@ -13,24 +13,24 @@ export default function Header() {
                 </NavbarBrand>
                 <NavbarContent className="hidden sm:flex gap-12 font-lato">
                     <NavbarItem>
-                        <Link href="#" className="text-white">
+                        <Link href="#" className="text-[#0F0F0F]">
                             Features
                         </Link>
                     </NavbarItem>
                     <NavbarItem>
-                        <Link href="#" className="text-white">
+                        <Link href="#" className="text-[#0F0F0F]">
                             Customers
                         </Link>
                     </NavbarItem>
                     <NavbarItem>
-                        <Link href="#" className="text-white">
+                        <Link href="#" className="text-[#0F0F0F]">
                             Integrations
                         </Link>
                     </NavbarItem>
                 </NavbarContent>
             </NavbarContent>
             <NavbarItem>
-                <Button className="text-white" as={Link} color="default" href="/login" variant="bordered">
+                <Button className="text-[#0F0F0F]" as={Link} color="default" href="/login" variant="bordered">
                     Sign Up
                 </Button>
             </NavbarItem>
