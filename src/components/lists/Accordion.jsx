7dabbedc44d9ccdb8fa5d7e704545921677a9649
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2023-11-09 01:48:20 -0600

    feat(pages):darker lists

--- src/components/lists/Accordion.jsx
+++ src/components/lists/Accordion.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Accordion, AccordionItem } from "@nextui-org/react";
 
-export default function List({ title, subtitle, defaultContent }) {
+export default function List({ title, subtitle, defaultContent, color }) {
   return (
     <Accordion>
-      <AccordionItem key="1" aria-label={`Accordion ${title}`} subtitle={subtitle} title={title} defaultContent={defaultContent} className="ml-5 " >
+      <AccordionItem  key="1" aria-label={`Accordion ${title}`} subtitle={subtitle} title={title} defaultContent={defaultContent} className="ml-5 text-white">
         {defaultContent}
       </AccordionItem>
     </Accordion>
--- src/pages/Lists.jsx
+++ src/pages/Lists.jsx
@@ -6,7 +6,7 @@ const Lists = () => {
     return (
         <>
             <Header />
-            <div className="">
+            <div className="bg-[#0F0F0F] dark">
                 <div className='mb-10'>
                     <div className='w-full h-[300px] bg-[url(./andres/activitiesimg1.png)] bg-cover bg-center'>
                         <h1 className='font-inter pt-[100px] ml-10 text-white text-7xl uppercase'>Secretarias del estado</h1>
@@ -19,28 +19,28 @@ const Lists = () => {
                         <div className='bg-[url(./andres/listsimg3.png)] w-[2px] h-[80px]'></div>
                         <List title="Secretaria de educación" subtitle="Conoce más" defaultContent="Dedicada a la educación"/>
                     </div>
-                    <div className='bg-[url(./andres/listsimg4.png)] w-[1000px] h-[1px] mt-4'></div>
+                    <div className='bg-[url(./andres/listsimg3.png)] w-[1000px] h-[1px] mt-4'></div>
 
                     <div className='w-[900px] flex text-sm mt-10 mb-4'>
                         <div className='bg-[url(./andres/listsimg2.svg)] w-[215px] h-[75px] mt-2 bg-cover ml-8 mr-[40px]'></div>
                         <div className='bg-[url(./andres/listsimg3.png)] w-[1px] h-[80px]'></div>
                         <List title="Secretaria de salud" subtitle="Conoce más"  defaultContent="Dedicada a la salud"/>
                     </div>
-                    <div className='bg-[url(./andres/listsimg4.png)] w-[1000px] h-[1px] mt-4'></div>
+                    <div className='bg-[url(./andres/listsimg3.png)] w-[1000px] h-[1px] mt-4'></div>
 
                     <div className='w-[900px] flex text-sm mt-8 mb-4'>
                         <div className='bg-[url(./andres/listsimg6.svg)] w-[265px] h-[75px] bg-cover mr-[38px]'></div>
                         <div className='bg-[url(./andres/listsimg3.png)] w-[1px] h-[80px]'></div>
                         <List title="Secretaria de obras públicas" subtitle="Conoce más" defaultContent="Dedicada a las obras públicas" />
                     </div>
-                    <div className='bg-[url(./andres/listsimg4.png)] w-[1000px] h-[1px] mt-4'></div>
+                    <div className='bg-[url(./andres/listsimg3.png)] w-[1000px] h-[1px] mt-4'></div>
 
                     <div className='w-[900px] flex text-sm mt-8 mb-4'>
                         <div className='bg-[url(./andres/listsimg7.svg)] w-[220px] h-[80px] bg-cover ml-8 mr-10'></div>
                         <div className='bg-[url(./andres/listsimg3.png)] w-[2px] h-[80px]'></div>
                         <List title="Secretaria de turismo" subtitle="Conoce más" defaultContent="Dedicada al turismo" />
                     </div>
-                    <div className='bg-[url(./andres/listsimg4.png)] w-[1000px] h-[1px] mt-4 mb-10'></div>
+                    <div className='bg-[url(./andres/listsimg3.png)] w-[1000px] h-[1px] mt-4 mb-10'></div>
                 </div>
 
             </div>
