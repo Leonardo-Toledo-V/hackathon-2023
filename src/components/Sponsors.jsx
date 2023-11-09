31aade9a687f3feb5307348a41196aea200b40c0
Leonardo-Toledo-V <leo.toledo.grams@gmail.com>
2023-11-09 00:02:54 -0600

    feat(components): Main

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 export default function Sponsors() {
     return (
         <div className='flex justify-center items-center '>
-            <div className='flex justify-center items-center mx-12 gap-16'>
+            <div className='flex justify-center items-center mx-12 gap-11'>
                 <img className='w-28' src="upgch.png" alt="upgch" />
                 <img className='w-44' src="chiapas.png" alt="chiapas" />
                 <img className='w-44 h-10' src="crypto.png" alt="crypto" />
                 <img className='w-44 h-14' src="innova.png" alt="innova" />
                 <img className='w-44 h-14' src="bio.png" alt="innova" />
+                <img className='w-40 h-14' src="vara.png" alt="vara" />
             </div>
         </div>
     )
