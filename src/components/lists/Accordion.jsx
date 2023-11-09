6b1f08b923dc0e1d06cbc5fbeff35ab5ba0e9722
andresjuarez1 <213364@ids.upchiapas.edu.mx>
2023-11-08 20:46:21 -0600

    feat(pages and components):adding titles and changes in the according

--- src/components/lists/Accordion.jsx
+++ src/components/lists/Accordion.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { Accordion, AccordionItem } from "@nextui-org/react";
 
-export default function List({ title, subtitle }) {
-  const defaultContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-
+export default function List({ title, subtitle, defaultContent }) {
   return (
     <Accordion>
-      <AccordionItem key="1" aria-label={`Accordion ${title}`} subtitle={subtitle} title={title} className="ml-5">
+      <AccordionItem key="1" aria-label={`Accordion ${title}`} subtitle={subtitle} title={title} defaultContent={defaultContent} className="ml-5" >
         {defaultContent}
       </AccordionItem>
     </Accordion>
